4b4ff901014d7055a38423df654f0c0a1652b99d
Patryk Tur <patryk.tur@gmail.com>
2019-04-26 21:30:45 +0200

    Add menu bar and menu

--- src/main/java/tur/patryk/TxtEditorSwing.java
+++ src/main/java/tur/patryk/TxtEditorSwing.java
@@ -1,6 +1,8 @@
 package tur.patryk;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class TxtEditorSwing {
 
@@ -12,7 +14,22 @@ public class TxtEditorSwing {
         frame.setContentPane(new TxtEditorSwing().panelMain);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
-        frame.setVisible(true);
-    }
+        //frame.setVisible(true);
 
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("File");
+        JMenuItem menuChooseFolder = new JMenuItem("Choose folder");
+        JMenuItem menuExit = new JMenuItem("Exit");
+        menu.add(menuChooseFolder);
+        menu.add(menuExit);
+        menuBar.add(menu);
+        frame.setJMenuBar(menuBar);
+        frame.setVisible(true);
+
+        menuExit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    }
 }
